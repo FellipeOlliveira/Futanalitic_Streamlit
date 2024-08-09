b2a892b57ab9a49754ca584537965dc059c62ce1
Marco <marcosf1625@gmail.com>
2024-08-08 22:41:54 -0300

    commits testes

--- engine/teste.py
+++ engine/teste.py
@@ -1,11 +1,10 @@
 import requests
-from bs4 import BeautifulSoup
 
 def requisicao_test() -> str:
     test = requests.get('https://www.google.com.br/')
     result = test.status_code
     if result == 200:
-        content = BeautifulSoup(test.content,'html.parser')
+        #content = BeautifulSoup(test.content,'html.parser')
         return 'Sucesso'
     elif result == 404:
         return 'Deu Errado'

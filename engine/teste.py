0844a0e6c7683904bb39518bf67faa67fcbb853a
Marco <marcosf1625@gmail.com>
2024-08-08 22:40:56 -0300

    inicial commit

--- engine/teste.py
+++ engine/teste.py
@@ -5,6 +5,7 @@ def requisicao_test() -> str:
     test = requests.get('https://www.google.com.br/')
     result = test.status_code
     if result == 200:
+        content = BeautifulSoup(test.content,'html.parser')
         return 'Sucesso'
     elif result == 404:
         return 'Deu Errado'

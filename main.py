aa08c36c85e7951eaa375698d67f57881bf4fcac
Marco <marcosf1625@gmail.com>
2024-08-08 21:55:40 -0300

    commit teste

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from src import teste as teste
+
+# Criação do menu lateral
+st.sidebar.header("Menu de Análise")
+
+# Select option
+opcao = st.sidebar.selectbox("Escolha uma opção:", ["Opção 1", "Opção 2", "Opção 3"])
+
+
+# Conteúdo principal da página
+st.title("Futanalitic")
+st.write("Utilize o menu lateral para inserir os dados e realizar a análise.")
+
+# Botão de Analisar
+if st.sidebar.button("Analisar"):
+    result = teste.requisicao_test()
+    st.write(f"Resultado do Teste : {result}")
---
+++ src/teste.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+
+def requisicao_test() -> str:
+    test = requests.get('https://www.google.com.br/')
+    result = test.status_code
+    if result == 200:
+        return 'Sucesso'
+    elif result == 404:
+        return 'Deu Errado'
+
+    else:
+        return 'Ai é foda'

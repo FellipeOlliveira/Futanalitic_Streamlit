ad6a2d24b02c00c244c6b58d7ea72faad5dae059
Marco <marcosf1625@gmail.com>
2024-08-10 11:21:29 -0300

    commits testes

--- engine/coletor.py
+++ engine/coletor.py
@@ -9,6 +9,7 @@ def coletar_partidas_time(liga_selecionada):
 
     jogos_html = BeautifulSoup(jogos_liga.text,'html.parser')
 
+    print(jogos_liga)
     jogos_links = jogos_html.find_all('//div[4]/table[1]/tbody/tr/td/table[1]//tr/td/a[@class="vsmall"]')
 
     return jogos_links

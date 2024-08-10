6883b214d845c7e2e8b53afd66c8fab12f9ef14a
Marco <marcosf1625@gmail.com>
2024-08-10 08:55:44 -0300

    commits testes

---
+++ utils/ligas.py
@@ -0,0 +1,15 @@
+ligas_disponiveis = {
+    'Alemanha Bundesliga':'austria',
+    'Alemanha Bundesliga2':'austria2',
+    'Austria A-league':'australia',
+    'Croatia HNL':'croatia',
+    'Espanha La Liga':'spain',
+    'France La Liga':'france',
+    'England Premier League':'england',
+    'England Championship':'england2',
+    'Netherlands Eredivisie':'netherlands',
+    'Poland Ekstraklasa':'poland',
+    'Portugal League 1':'portugal',
+    'Republica Tcheca Foruna League':'czechrepublic',
+    'Turkey Super League':'turkey'
+}

d2c5f3de94abeb3fa3add57f2c358c1d77e71562
Marco <marcosf1625@gmail.com>
2024-08-10 11:21:55 -0300

    commits testes

--- engine/coletor.py
+++ engine/coletor.py
@@ -5,11 +5,10 @@ def coletar_partidas_time(liga_selecionada):
     #https://www.soccerstats.com/latest.asp?league=
     session = requests.session()
 
-    jogos_liga = session.get(f'https://www.soccerstats.com/latest.asp?league=australia4')
+    jogos_liga = session.get(f'https://www.soccerstats.com/latest.asp?league={liga_selecionada}')
 
     jogos_html = BeautifulSoup(jogos_liga.text,'html.parser')
 
-    print(jogos_liga)
     jogos_links = jogos_html.find_all('//div[4]/table[1]/tbody/tr/td/table[1]//tr/td/a[@class="vsmall"]')
 
     return jogos_links
